0f0431e825c6ccac086ca8f0f728401e51887295
yirdhayani/kiddo.github.io <yirdhayani@gmail.com>
2026-05-15 17:32:12 +0700

    Menambahkan interaksi JavaScript pada tombol utama

---
+++ script.js
@@ -0,0 +1,5 @@
+const tombol = document.querySelector('.btn-utama');
+
+tombol.addEventListener('click', function() {
+    alert('Selamat datang! Kamu baru saja menjalankan kode JavaScript pertamamu. 🚀');
+});
